467a44c0122d70f8393838ea56c6730265191745
PRANATHI83 <apranu8@gmail.com>
2025-07-07 12:45:34 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3003;
+const port = 3206;
 
 // Middleware
 app.use(cors());
@@ -13,7 +13,7 @@ const pool = new Pool({
     user: 'postgres', // Replace with your PostgreSQL username
     host: 'postgres',
     database: 'job_portal',
-    password: 'admin234', // Replace with your PostgreSQL password
+    password: 'admin123', // Replace with your PostgreSQL password
     port: 5432,
 });
 
@@ -119,5 +119,5 @@ app.post('/api/jobs', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://13.201.36.187:${port}`);
+    console.log(`Server running at http://3.85.61.23:${port}`);
 });
